cdf145d48faaa2a3de51918be02162d919829d22
Satoshi Warita <stm@stmtok.com>
2023-09-12 17:53:56 +0900

    remove unused imports

--- src/main/java/jp/sagalab/MyPanel.java
+++ src/main/java/jp/sagalab/MyPanel.java
@@ -14,12 +14,7 @@ import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 import static java.lang.Math.ceil;
 import static java.lang.Math.max;
